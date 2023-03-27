33b2904a585d6787819225bb0c0fbf2896154475
John Comeau <jc@unternet.net>
2023-03-26 19:56:30 -0700

    _doprnt now supports `make -f _doprnt.mk test`

--- libiberty/_doprnt.mk
+++ libiberty/_doprnt.mk
@@ -1,7 +1,10 @@
+SHELL := /bin/bash
 _doprnt: _doprnt.o ./libiberty.a .FORCE
 	gcc $< -o $@ $(word 2, $+)
 _doprnt.o: .FORCE
 	$(MAKE) CFLAGS='-DTEST=1' _doprnt.o
 libiberty.a:
 	$(MAKE) libiberty.a
+test: _doprnt
+	diff <(./$< 2>&1 >/dev/null) <(./$< 2>/dev/null)
 .FORCE:
