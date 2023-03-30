416be3b519f3e802457ab17125fc3d46e7676e91
John Comeau <jc@unternet.net>
2023-03-29 17:58:39 -0700

    the cast needed to be rewritten as (double)*&PI

--- libiberty/simpletest.c
+++ libiberty/simpletest.c
@@ -3,8 +3,8 @@
 int main (void) {
   const double PI = 3.1415926535897932385;
   const double *pi = &PI;
-  printf("&PI: %p, PI: %.16f (double *)&PI: %.16f\n",
-	    &PI, PI, *&PI);
+  printf("&PI: %p, PI: %.16f (double)*&PI: %.16f\n",
+	    &PI, PI, (double)*&PI);
   printf("pi: %p, *pi: %.16f\n", pi, *pi);
   return 0;
 }
