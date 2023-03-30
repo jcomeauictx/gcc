bad598507c91705e8a2ab2fd7f095df46351e657
John Comeau <jc@unternet.net>
2023-03-29 17:56:19 -0700

    the simple case worked!? the (double *) cast was what borked it.

--- libiberty/simpletest.c
+++ libiberty/simpletest.c
@@ -4,7 +4,7 @@ int main (void) {
   const double PI = 3.1415926535897932385;
   const double *pi = &PI;
   printf("&PI: %p, PI: %.16f (double *)&PI: %.16f\n",
-	    &PI, PI, (double *)&PI);
+	    &PI, PI, *&PI);
   printf("pi: %p, *pi: %.16f\n", pi, *pi);
   return 0;
 }
