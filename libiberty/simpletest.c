83169171da4a67fc0293e13ff6ff255f7c01def4
John Comeau <jc@unternet.net>
2023-03-30 20:24:33 -0700

    testing dereferencing double from char *

--- libiberty/simpletest.c
+++ libiberty/simpletest.c
@@ -1,10 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+char * doublestring(double number);
 
 int main (void) {
   const double PI = 3.1415926535897932385;
   const double *pi = &PI;
+  char *string_pi = doublestring(PI);
   printf("&PI: %p, PI: %.16f (double)*&PI: %.16f\n",
 	    &PI, PI, (double)*&PI);
   printf("pi: %p, *pi: %.16f\n", pi, *pi);
+  printf("string_pi: \"%s\" (%.16f)\n", string_pi, (double) *string_pi);
   return 0;
+}
+
+char * doublestring(double number) {
+  /* I'm not even sure such a thing is necessary, but so far have been stymied
+   * in inserting and retrieving doubles to/from compound literals. */
+  char *string = malloc(65);  /* at most 4 per digit (\000) plus final null */
+  /* we won't worry about `free`ing the memory, that will happen automatically
+   * at program termination */
+  int offset = 0;
+  int i;
+  unsigned char u, *ptr = (unsigned char *)&number;
+  /* let's do this for little-endian, work on endian-agnostic version later */
+  for (i = 0; i < 8; i++) {
+    u = ptr[i];
+    if (u < ' ' || u > '_') offset += sprintf(string + offset, "\\%03o", u);
+    else string[offset++] = u;
+    string[offset] = '\0';
+  }
+  return string;
 }
