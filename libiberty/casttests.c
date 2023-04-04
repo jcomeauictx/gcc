0aef413bed63f73d5e8c9b74583518fd38144a26
John Comeau <jc@unternet.net>
2023-04-04 15:20:39 -0700

    gives lots of warnings with -Wall

--- libiberty/casttests.c
+++ libiberty/casttests.c
@@ -5,5 +5,14 @@ int main() {
 	long ltest = (long)dtest;
 	void *vtest = (void *)ltest;
 	printf("test: %p %f %ld \"%s\"\n", test, dtest, ltest, (char *)vtest);
+	/* https://gcc.gnu.org/onlinedocs/gcc/Compound-Literals.html */
+	struct foo {int a; char b[2];} structure;
+	/* skip this one, it assumes predefined x and y */
+	/*structure = ((struct foo) {x + y, 'a', 0});*/
+	char **foo = (char *[]) { "x", "y", "z" };
+	int i = ++(int) { 1 };
+	static struct foo x = (struct foo) {1, 'a', 'b'};
+	static int y[] = (int []) {1, 2, 3};
+	static int z[] = (int [3]) {1};
 	return 0;
 }
